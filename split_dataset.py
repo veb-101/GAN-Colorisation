c6b270c2f1c82c658a2d2a94e92191a7834243d0
Vaibhav Singh <vaibhav.singh.3001@gmail.com>
2021-01-27 00:16:11 +0530

    created dataset

---
+++ split_dataset.py
@@ -0,0 +1,49 @@
+import gc
+import os
+import shutil
+from sklearn.model_selection import train_test_split
+
+ROOT_DIR = r"images_og"
+TRAIN = r"images/train"
+VALID = r"images/valid"
+
+
+def create_folder(folder):
+    if os.path.exists(folder):
+        shutil.rmtree(folder)
+    os.makedirs(folder)
+
+
+def copy_files(source, destination, files):
+    create_folder(destination)
+    len_ = len(files)
+    for idx, image_name in enumerate(files):
+        image_path = os.path.join(source, image_name)
+        dest_path = os.path.join(destination, image_name)
+        try:
+            shutil.copy(image_path, dest_path)
+        except:
+            print(image_path)
+            print(dest_path)
+
+        if not (idx+1) % 50 or idx+1 == len_:
+            print(f"Copied: {idx+1}/{len_} images", end="\r")
+
+    print()
+    gc.collect()
+    return
+
+
+if __name__ == '__main__':
+
+    image_names = os.listdir(ROOT_DIR)
+    length = len(image_names)
+
+    images_train, images_valid = train_test_split(
+        image_names, shuffle=True, random_state=41, test_size=64
+    )
+
+    print("Train images:", len(images_train), "Validation images:", len(images_valid))
+
+    copy_files(ROOT_DIR, TRAIN, images_train)
+    copy_files(ROOT_DIR, VALID, images_valid)
